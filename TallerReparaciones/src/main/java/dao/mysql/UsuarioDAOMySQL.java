c4c39a13c99ee27f085c7dd680ea9ba79aa12663
Elias EL Yousfi Aoudar <Wjo94412@educastur.es>
2025-11-11 22:08:24 +0100

    Commit 11/11

--- TallerReparaciones/src/main/java/dao/mysql/UsuarioDAOMySQL.java
+++ TallerReparaciones/src/main/java/dao/mysql/UsuarioDAOMySQL.java
@@ -1,15 +1,26 @@
 package dao.mysql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
+import dao.DBCconnection;
 import dao.interfaces.UsuarioDAO;
+import dwes.maven.mysql.PasswordUtils;
 import entities.Usuario;
 
 
-
-
 public class UsuarioDAOMySQL implements UsuarioDAO {
+	private Connection conexion;
+	
+	public UsuarioDAOMySQL() {
+		conexion = DBCconnection.getInstance().getConnection();
+	}
 
+	
     @Override
     public boolean login(String dni, String password) {
         // Aquí irá el código JDBC para comprobar usuario y contraseña
@@ -18,9 +29,45 @@ public class UsuarioDAOMySQL implements UsuarioDAO {
 
     @Override
     public int insert(Usuario u) {
-        // Código JDBC para insertar un usuario en la BD
-        return 0; // temporal
-    }
+    	try (Scanner sc = new Scanner(System.in)) {
+	        // Pedir datos por consola
+	        System.out.print("Introduce el ID del usuario: ");
+	        int idUsuario = sc.nextInt();
+	        sc.nextLine(); // limpiar el salto de línea
+
+	        System.out.print("Introduce el nombreUsuario: ");
+	        String nombreUsuario = sc.nextLine();
+
+	        System.out.print("Introduce el dni: ");
+	        int dni = sc.nextInt();
+	        sc.nextLine();
+
+	        System.out.print("Introduce el rol: ");
+	        String rol = sc.nextLine();
+
+	        System.out.print("Introduce la contraseña: ");
+	        String password = sc.nextLine();
+
+	        
+	        String sql = "INSERT INTO persona (idEmpleado, nombre, edad, numTelefono, fechaNacimiento, password) VALUES(?, ?, ?, ?, ?, ?);";
+	        PreparedStatement pst = conexion.prepareStatement(sql);
+
+	        pst.setInt(1, idUsuario);
+	        pst.setString(2, nombreUsuario);
+	        pst.setInt(3, dni);
+	        pst.setString(4, rol);
+	        pst.setString(6, PasswordUtils.hashPassword(password));
+
+	        int resul = pst.executeUpdate();
+	        System.out.println("Resultado de inserción: " + resul);
+	    } catch (SQLException e) {
+	        System.out.println("> NOK: " + e.getMessage());
+	    } catch (Exception e) {
+	        System.out.println("> Error: " + e.getMessage());
+	    }
+		return 0;
+	}
+    
 
     @Override
     public ArrayList<Usuario> findall() {
--- TallerReparaciones/src/main/java/vista/VistaTaller.java
+++ TallerReparaciones/src/main/java/vista/VistaTaller.java
@@ -1,5 +1,27 @@
 package vista;
 
+import dao.DAOFactory;
+import dao.MySQLDAOFactory;
+import dao.mysql.UsuarioDAOMySQL;
+
+
 public class VistaTaller {
-	
+	public static void main(String[] args) {
+
+		//PersonaDAOImpl personaDAO = DAOFactory.getPersonaDAO();
+		// personaDao.insertarPersona();
+		//personaDAO.recuperarPersonas();
+
+		// MySQLManager.insertarPersona(conexionMySQL);
+		// MySQLManager.recuperarPersonas(conexionMySQL);
+		// MySQLManager.borrarPersona(conexionMySQL);
+		// MySQLManager.modificarEdadPersonas(conexionMySQL);
+		
+
+		
+
+	}
+
 }
+
+
