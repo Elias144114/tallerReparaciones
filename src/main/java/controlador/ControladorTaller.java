7d1e2d724f20c6fa45cd714c0677f1879fa770c3
Elias2005 <elyousfielias2005@gmail.com>
2025-11-26 05:13:01 +0100

    Empece a trabajar en el controlador

--- src/main/java/controlador/ControladorTaller.java
+++ src/main/java/controlador/ControladorTaller.java
@@ -1,9 +1,106 @@
 package controlador;
 
+import java.util.ArrayList;
+
+import dao.mysql.*;
+import entities.*;
+
 
 
 public class ControladorTaller {
 
-    
- 
+	// *
+	private static ControladorTaller instance;
+
+	
+	private Usuario usuarioLogueado;
+
+	
+	private UsuarioDAOMySQL usuarioDAO;
+	private ClienteDAOMySQL clienteDAO;
+	private VehiculoDAOMySQL vehiculoDAO;
+	private ReparacionDAOMySQL reparacionDAO;
+
+	
+	private ControladorTaller() {
+		
+		usuarioDAO = new UsuarioDAOMySQL();
+		clienteDAO = new ClienteDAOMySQL();
+		vehiculoDAO = new VehiculoDAOMySQL();
+		reparacionDAO = new ReparacionDAOMySQL();
+	}
+
+	//Esto es para recordarme que este codigo y el de el * lo saque de la pagina Web que nos enseño Gonzalo, recuerda preguntarle que te lo explique un poco mejor
+	public static ControladorTaller getInstance() {
+	    if (instance == null) {
+	        instance = new ControladorTaller();
+	    }
+	    return instance;
+	}
+	
+	//INICIAR SESION 
+	
+	public boolean login(String nombreUsuario, String password) {
+		Usuario u = usuarioDAO.findByNombre(nombreUsuario);
+		if (u == null) {
+			 System.out.println("El usuario NO existe"); 
+		} else if (u.getPassword().equals(password)) {
+			this.usuarioLogueado=u;
+		    System.out.println("Nombre de usuario y contraseña correctas");
+		    return true;
+		} 		
+		return false;
+
+	}
+	
+	 public void logout() {
+	        this.usuarioLogueado = null;
+	    }
+
+	    public boolean sesionActiva() {
+	        return usuarioLogueado != null;
+	    }
+
+	    public Usuario getUsuarioLogueado() {
+	        return usuarioLogueado;
+	    }
+	    
+	    
+	    // VER REPARACIONES
+	
+	public ArrayList<Reparacion> ReparacionesFinalizadas() {
+
+		ArrayList<Reparacion> reparaciones = reparacionDAO.findall();
+
+		System.out.println(" ");
+		System.out.println("           LISTADO DE REPARACIONES              ");
+		System.out.println(" ");
+
+		if (reparaciones.isEmpty()) {
+			System.out.println("No hay reparaciones en la base de datos.");
+
+		}
+
+		for (Reparacion r : reparaciones) {
+			System.out.println("ID: " + r.getIdReparacion());
+			System.out.println("  Descripción: " + r.getDescripcion());
+			System.out.println("  Fecha de entrada: " + r.getFechaEntrada());
+			System.out.println("  Coste estimado: " + r.getCosteEstimado());
+			System.out.println("  Estado: " + r.getEstado());
+			System.out.println("  Id del Vehiculo: " + r.getVehiculoId());
+			System.out.println("  Id del Usuario: " + r.getUsuarioId());
+			System.out.println("");
+		}
+		return reparaciones;
+	}
+	
+	 public boolean registrarReparacion(Reparacion r) {
+		//Aqui me lie y me fui a dormir para preguntar la duda mañana, por si no me acuerdo, es lo de Vista-Fachada y pedir por consola o si no es necesario
+		 
+		 
+	        return false;
+	    }
+
+	    
+	
 }
--- src/main/java/dao/mysql/ClienteDAOMySQL.java
+++ src/main/java/dao/mysql/ClienteDAOMySQL.java
@@ -11,13 +11,13 @@ import dao.DBCconnection;
 import dao.interfaces.ClienteDAO;
 import entities.Cliente;
 
-public class ClienteDAOMySQL implements ClienteDAO{
-private Connection conexion;
-	
+public class ClienteDAOMySQL implements ClienteDAO {
+	private Connection conexion;
+
 	public ClienteDAOMySQL() {
 		conexion = DBCconnection.getInstance().getConnection();
 	}
-	
+
 	@Override
 	public int insert(Cliente c) {
 		int resul = 0;
@@ -31,7 +31,7 @@ private Connection conexion;
 			pst.setString(3, c.getNombre());
 			pst.setString(4, c.getTelefono());
 			pst.setString(5, c.getEmail());
-			
+
 			resul = pst.executeUpdate();
 			System.out.println("Resultado de inserción: " + resul);
 		} catch (SQLException e) {
@@ -43,8 +43,7 @@ private Connection conexion;
 		}
 		return resul;
 	}
-	
-	
+
 	@Override
 	public int update(Cliente c) {
 		int resul = 0;
@@ -52,13 +51,12 @@ private Connection conexion;
 
 			String sql = "UPDATE cliente SET nombre = ?, telefono = ?, email = ? WHERE dni = ?;";
 			PreparedStatement pst = conexion.prepareStatement(sql);
-				
-			
+
 			pst.setString(1, c.getNombre());
 			pst.setString(2, c.getTelefono());
 			pst.setString(3, c.getEmail());
 			pst.setString(4, c.getDni());
-			
+
 			resul = pst.executeUpdate();
 			System.out.println("Resultado de inserción: " + resul);
 		} catch (SQLException e) {
@@ -68,78 +66,77 @@ private Connection conexion;
 		}
 		return resul;
 	}
-	
+
 	@Override
 	public int delete(String dni) {
 		String sqlDelete = "DELETE FROM cliente WHERE dni = ?;";
 		try {
 			PreparedStatement pst = conexion.prepareStatement(sqlDelete);
-			pst.setString(1, dni); 
+			pst.setString(1, dni);
 			int filas = pst.executeUpdate();
-			
+
 			if (filas > 0) {
 				System.out.println("> OK. Persona con dni " + dni + " eliminada correctamente.");
 			} else {
 				System.out.println("> NOK. Persona con dni " + dni + " no se encuentra en la base de datos.");
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return 0;
 	}
-	
-	  @Override
-	    public ArrayList<Cliente> findall() {
-		  ArrayList<Cliente> clientes = new ArrayList<>();
-		    String sql = "SELECT idCliente, dni, nombre, telefono, email FROM cliente;";
-		    try (PreparedStatement pst = conexion.prepareStatement(sql);
-		         ResultSet resul = pst.executeQuery()) {
 
-		        // 1. Recorrer el ResultSet
-		        while (resul.next()) {
-		            // 2. Crear un objeto Cliente por cada fila
-		            Cliente c = new Cliente();
-		            c.setIdCliente(resul.getInt("idCliente"));
-		            c.setDni(resul.getString("dni"));
-		            c.setNombre(resul.getString("nombre"));
-		            c.setTelefono(resul.getString("telefono"));
-		            c.setEmail(resul.getString("email"));
-		            
-		            
-		            clientes.add(c);
-		        }
-		    } catch (SQLException e) {
-		        System.out.println("> NOK en findall: " + e.getMessage());
-		        e.printStackTrace();
-		    }
-		    return clientes;
+	@Override
+	public ArrayList<Cliente> findall() {
+		ArrayList<Cliente> clientes = new ArrayList<>();
+		String sql = "SELECT idCliente, dni, nombre, telefono, email FROM cliente;";
+		try (PreparedStatement pst = conexion.prepareStatement(sql); 
+				ResultSet resul = pst.executeQuery()) {
+
+			// 1. Recorrer el ResultSet
+			while (resul.next()) {
+				
+				Cliente c = new Cliente();
+				c.setIdCliente(resul.getInt("idCliente"));
+				c.setDni(resul.getString("dni"));
+				c.setNombre(resul.getString("nombre"));
+				c.setTelefono(resul.getString("telefono"));
+				c.setEmail(resul.getString("email"));
+
+				clientes.add(c);
+			}
+		} catch (SQLException e) {
+			System.out.println("> NOK en findall: " + e.getMessage());
+			e.printStackTrace();
 		}
-	
-	  @Override
-	    public Cliente findByDni(String dni) {
-		  Cliente cliente = null;
-		    String sql = "SELECT idCliente, dni, nombre, telefono, email FROM cliente WHERE dni = ?;";
-		    
-		    try (PreparedStatement pst = conexion.prepareStatement(sql)) {
-		        
-		        pst.setString(1, dni);
-		        
-		        try (ResultSet resul = pst.executeQuery()) {
-		            if (resul.next()) {
-		                cliente = new Cliente();
-		                cliente.setIdCliente(resul.getInt("idCliente"));
-		                cliente.setDni(resul.getString("dni"));
-		                cliente.setNombre(resul.getString("nombre"));
-		                cliente.setTelefono(resul.getString("telefono"));
-		                cliente.setEmail(resul.getString("email"));
-		            }
-		        }
-		    } catch (SQLException e) {
-		        System.out.println("> NOK en findByDni: " + e.getMessage());
-		        e.printStackTrace();
-		    }
-		    return cliente;
+		return clientes;
+	}
+
+	@Override
+	public Cliente findByDni(String dni) {
+		Cliente cliente = null;
+		String sql = "SELECT idCliente, dni, nombre, telefono, email FROM cliente WHERE dni = ?;";
+
+		try (PreparedStatement pst = conexion.prepareStatement(sql)) {
+
+			pst.setString(1, dni);
+
+			try (ResultSet resul = pst.executeQuery()) {
+				if (resul.next()) {
+					cliente = new Cliente();
+					cliente.setIdCliente(resul.getInt("idCliente"));
+					cliente.setDni(resul.getString("dni"));
+					cliente.setNombre(resul.getString("nombre"));
+					cliente.setTelefono(resul.getString("telefono"));
+					cliente.setEmail(resul.getString("email"));
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("> NOK en findByDni: " + e.getMessage());
+			e.printStackTrace();
 		}
+		return cliente;
+	}
 }
